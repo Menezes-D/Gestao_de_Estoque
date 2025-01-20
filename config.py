5f399d2a40ed2ead80396c63313d5f9d3d5c8b20
Menezes-D <mnzs.douglas@gmail.com>
2025-01-20 03:22:46 -0300

    HTML e CSS

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SQLACHEMY_DATABASE_URI = 'mysql+pymysql://root2:usbw@localhost/test'
+    SQLACHEMY_DATABASE_URI = 'mysql+pymysql://root2:Defcon4385@localhost/estoque'
