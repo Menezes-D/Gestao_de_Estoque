0f451829f62573bb283767e2c122092e5c471c2e
Menezes-D <mnzs.douglas@gmail.com>
2025-01-28 03:59:43 -0300

    Aplicação Finalizada

--- app/models.py
+++ app/models.py
@@ -11,7 +11,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
-    role = db.Column(db.String(50), nullable=False)  # "user" ou "admin"
+    role = db.Column(db.String(50), nullable=False)  
 
     def set_password(self, password):
         self.password = generate_password_hash(password)  # Criptografa a senha
--- app/routes.py
+++ app/routes.py
@@ -17,17 +17,17 @@ def index():
     
     # Se o usuário não estiver logado, redireciona para o login
     if not current_user.is_authenticated:
-        return redirect(url_for('login'))  # Redireciona para a página de login
+        return redirect(url_for('login')) 
     
     return render_template('index.html', items=items_)
 
 @app.route('/create', methods=['GET', 'POST'])
-@login_required  # Garante que somente usuários logados possam acessar
+@login_required  # Garante que somente usuários logados possam acessar essa rota
 def create():
-    # Verifica se o usuário é o admin
+    
     if current_user.role != 'admin':  
         flash('Você não tem permissão para acessar esta página!', 'error')
-        return redirect(url_for('index'))  # Usuário não tem permissão, redireciona para o início
+        return redirect(url_for('index'))  # Se o usuário não tiver permissão, redireciona para o início
     
     if request.method == 'POST':
         nome = request.form['nome']
@@ -43,7 +43,7 @@ def create():
 @login_required  
 def update(id):
     if current_user.role != 'admin':  
-        flash('Você não tem permissão para acessar esta págin!', 'error')
+        flash('Você não tem permissão para acessar esta página!', 'error')
         return redirect(url_for('index')) 
     
     item = Produtos.query.get_or_404(id)
@@ -56,7 +56,7 @@ def update(id):
     return render_template('update.html', item=item)
 
 @app.route('/delete/<int:id>')
-@login_required  # Garante que somente usuários logados possam acessar
+@login_required  
 def delete(id):
     if current_user.role != 'admin':  
         flash('Você não tem permissão para acessar esta página!', 'error')
@@ -69,11 +69,11 @@ def delete(id):
     return redirect(url_for('index'))
 
 @app.route('/logout')
-@login_required  # Garante que somente usuários logados possam acessar
+@login_required  
 def logout():
-    logout_user()  # Logout do usuário
+    logout_user()  
     flash('Você foi desconectado.', 'info')
-    return redirect(url_for('login'))  # Redireciona para a página de login
+    return redirect(url_for('login'))  
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -96,38 +96,38 @@ def login():
             if next_page:
                 return redirect(next_page)
             else:
-                return redirect(url_for('index'))  # Caso contrário, redireciona para a página inicial
+                return redirect(url_for('index'))  
 
-        flash('Usuário ou senha inválidos', 'danger')
+        flash('Usuário ou senha inválidos', 'error')
     
     return render_template('login.html', form=form)
 
 @app.route('/register', methods=['GET', 'POST'])
-@login_required  # Garante que apenas usuários logados possam acessar
+@login_required  
 def register():
-    if current_user.role != 'admin':  # Verifica se o usuário é administrador
+    if current_user.role != 'admin':  
         flash('Você não tem permissão para acessar esta página!', 'error')
-        return redirect(url_for('index'))  # Caso contrário, redireciona para a página inicial
+        return redirect(url_for('index'))  
     
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        role = request.form['role']  # Pode ser "admin" ou "user"
+        role = request.form['role']  
         
-        # Verifica se o username já existe
+        # Verifica se o nome de usuário já existe
         existing_user = User.query.filter_by(username=username).first()
         if existing_user:
             flash('Usuário ja cadastrado!', 'error')
-            return render_template('register.html')  # Retorna o formulário de registro com mensagem de erro
+            return render_template('register.html')  
 
         # Cria o novo usuário
         new_user = User(username=username, role=role)
-        new_user.set_password(password)  # Criptografa a senha
+        new_user.set_password(password)  
 
         db.session.add(new_user)
         db.session.commit()
         
         flash('Novo usuário cadastrado com sucesso!', 'success')
-        return redirect(url_for('index'))  # Redireciona para a página principal após sucesso
+        return redirect(url_for('index'))  
     
-    return render_template('register.html')  # Retorna o formulário de cadastro
+    return render_template('register.html')  
