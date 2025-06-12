710ed286cdb7e32adbbae5e6ff6fa5c7b232cea5
Menezes-D <130506713+Menezes-D@users.noreply.github.com>
2025-06-11 22:00:36 -0300

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Defcon4385@localhost/estoque'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:PASSWORD@localhost/estoque'
